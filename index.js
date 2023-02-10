c8a7d62f5953ae789194b9acd341c9c0df7b24db
Weekiat553 <116060902+Weekiat553@users.noreply.github.com>
2023-02-10 09:28:54 +0800

    API sign up

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-$(document).ready(function() {
-    //what kind of interface we want at the start 
-    const APIKEY = "63d9e16d3bc6b255ed0c4502";
-
-    //[STEP 1]: Create our submit form listener
-    $("#user-submit").on("click", function(e) {
-      //prevent default action of the button 
-      e.preventDefault();
+$(document).ready(function () {
+    const APIKEY = "63e208533bc6b255ed0c474d";
+    getContacts();
   
-      //[STEP 2]: let's retrieve form data
-      //for now we assume all information is valid
-      //you are to do your own data validation
-      let userEmail = $("#eMail").val();
-      let userPassword = $("#paSsword").val();
-      let userName = $("#uSername").val();
+    $("#start").on("click", function (e) {
+      e.preventDefault();
+      console.log('hello');
+      
+      let email = $("#email").val();
+      let password = $("#password").val();
+      let username = $("#username").val();
   
       //[STEP 3]: get form values when user clicks on send
       //Adapted from restdb api
       let jsondata = {
-        "email": userEmail,
-        "password": userPassword,
-        "username": userName
+        "email": email,
+        "password": password,
+        "username": username
       };
   
-      //[STEP 4]: Create our AJAX settings. Take note of API key
       let settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://quickies-beb5.restdb.io/rest/userregistration",
+        "url": "https://quickies-168a.restdb.io/rest/register",
         "method": "POST", //[cher] we will use post to send info
         "headers": {
           "content-type": "application/json",
@@ -35,20 +30,21 @@ $(document).ready(function() {
         },
         "processData": false,
         "data": JSON.stringify(jsondata),
-        "beforeSend": function() {
-          //@TODO use loading bar instead
-          //disable our button or show loading bar
-          $("#user-submit").prop("disabled", true);
-          //clear our form using the form id and triggering it's reset feature
-        }
       }
-  
-    }); //end click
+      $.ajax(settings).done(function (response) {
+        console.log(response);
 
-      //[STEP] 6
-  //let's create a function to allow you to retrieve all the information in your contacts
-  //by default we only retrieve 10 results
-  function getUser(limit = 10, all = true) {
+        $("#start").prop( "disabled", false);
+        getContacts();
+    }); 
+});
+
+function getContacts(limit = 10, all = true) {
 
   }
 })
+
+
+function auth(){
+  window.location.assign('latest.html')
+}
